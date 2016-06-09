8e6981aa50520111c60056a0b109a4a9f0c1399e
Winston Mi <winstonmichmi@gmail.com>
2016-06-09 11:03:49 +0800

    version1 with gifs, mrt and code puns

--- project1a.js
+++ project1a.js
@@ -1,53 +1,75 @@
-
+$(document).ready(function(){
 
 var questions = [
   {
-    question : 'I knows, and ________ we will get through this course.',
-    answerIndex : 2, // indicates the correct array number, use 0, 1...
-    choices  : ['Chinatown', 'Bugis', 'Eunos', 'Potong']
+    question : 'When you cannot understand any of the lab and ask your friend to ________ it to you.',
+    answerIndex : 1, // indicates the correct array number, use 0, 1...
+    choices  : ['Chinatown', 'Esplanade', 'Eunos', 'Potong'],
     // answers  : [ "The correct answer is True"]
+    gif : 'gifs/two-girls.gif'
   },
    {
-    question : 'When you think you right, and then look at your code with Linter and ________.',
+    question : "When you've been sitting in class too long and you really need to pee because your ________ is full.",
     answerIndex : 2,
-    choices  : ['Clemnti', 'Queenstown', 'Jurong', 'Bukit Batok'],
+    choices  : ['Clementi', 'Queenstown', 'Braddell', 'Bukit Batok'],
     // answers  : [ 'The correct answer is True']
+    gif : 'gifs/2-braddell.gif'
   },
   {
-    question : 'During project week, my heart goes ________?',
-    answerIndex : 1, // indicates the correct array number, use 0, 1...
-    choices  : ['Rochor', 'Bedok, Bedok', 'Serangoon', 'Dhoby Ghaut']
+    question : 'When you’re running late to class and you know you ________ in time.',
+    answerIndex : 3, // indicates the correct array number, use 0, 1...
+    choices  : ['Rochor', 'Bedok', 'Serangoon', 'Kent Ridge'],
     // answers  : [ "The correct answer is True"]
+    gif : 'gifs/3-kent-ridge.gif'
   },
   {
-    question : "When you've been sitting in class too long and you really need to pee because your ________ is full.",
+    question : "When you think you're right, and look at your code with Linter and ________.",
     answerIndex : 1, // indicates the correct array number, use 0, 1...
-    choices  : ['Telok Blangah', 'Braddell', 'Stevens', 'Marymount']
+    choices  : ['Tampines', 'Jurong', 'Stevens', 'Marymount'],
     // answers  : [ "The correct answer is True"]
+    gif : 'gifs/crying.gif'
   },
   {
-    question : "When you cannot understand the lab and ask your friend to ________ it to you.",
+    question : 'When you drink 4 kopis a day during project week, your heart goes ________?',
+    answerIndex : 1, // indicates the correct array number, use 0, 1...
+    choices  : ['Esplanade', 'Bedok', 'Marina Bay', 'Admiralty'],
+    // answers  : [ "The correct answer is True"]
+    gif : 'gifs/2-braddell.gif'
+  },
+  {
+    question : "When it’s 6pm on Friday and you know the week is all ________.",
+    answerIndex : 2, // indicates the correct array number, use 0, 1...
+    choices  : ['Telok Blangah', 'Mountbatten', 'Dover', 'Bartley'],
+    // answers  : [ "The correct answer is True"]
+    gif : 'gifs/6-dover.gif'
+  },
+  {
+    question : 'When our class shows up to the kopitiam and the server says, "There’s a ________ of them!"',
     answerIndex : 0, // indicates the correct array number, use 0, 1...
-    choices  : ['Esplanade', 'Yew Tee', 'Marina Bay', 'Admiralty']
+    choices  : ['Hougang', 'Stadium', 'Stevens', 'Dakota'],
     // answers  : [ "The correct answer is True"]
+    gif : 'gifs/gang.gif'
   },
   {
-    question : "When you've been sitting in class too long and you really need to pee because your ________ is full.",
-    answerIndex : 1, // indicates the correct array number, use 0, 1...
-    choices  : ['Telok Blangah', 'Braddell', 'Stevens', 'Marymount']
+    question : "When you write good code and your classmate asks you, 'Can ________ -ch me?'",
+    answerIndex : 2, // indicates the correct array number, use 0, 1...
+    choices  : ['Queenstown', 'Bras Basah', 'Yew Tee', 'Rochor'],
     // answers  : [ "The correct answer is True"]
+    gif : 'gifs/flex.gif'
   },
   {
-    question : "When you've been sitting in class too long and you really need to pee because your ________ is full.",
-    answerIndex : 1, // indicates the correct array number, use 0, 1...
-    choices  : ['Telok Blangah', 'Braddell', 'Stevens', 'Marymount']
+    question : 'When it’s 3am and you’re still coding, ________ is love.',
+    answerIndex : 3, // indicates the correct array number, use 0, 1...
+    choices  : ['Buona Vista', 'HarbourFront', 'Redhill', 'Aljunied'],
     // answers  : [ "The correct answer is True"]
+    gif : 'gifs/smiling.gif'
   },
   {
-    question : "When you've been sitting in class too long and you really need to pee because your ________ is full.",
-    answerIndex : 1, // indicates the correct array number, use 0, 1...
-    choices  : ['Telok Blangah', 'Braddell', 'Stevens', 'Marymount']
+    question : 'Everything is going to be okay. I knows, and ________ we will get through this course together.',
+    answerIndex : 3, // indicates the correct array number, use 0, 1...
+    choices  : ['Hillview', 'Bayfron', 'Downtown', 'Eunos'],
     // answers  : [ "The correct answer is True"]
+    gif : 'gifs/elevator.gif'
   }
 ]
 
@@ -83,6 +105,7 @@ function numberOfAnswers() {
 // It should return an integer that is the number of choices for the current question
 //
 function playTurn(choice) {
+  console.log(currentQuestion())
   if(isGameOver()) {
     return
   }
@@ -96,6 +119,18 @@ function playTurn(choice) {
       quiz.player2Score++
     }
   }
+  if (quiz.currentQ === numberOfQuestions()) {
+  $('.ansChoice').eq(0).toggle()
+  $('.ansChoice').eq(1).toggle()
+  $('.ansChoice').eq(2).toggle()
+  $('.ansChoice').eq(3).toggle()
+  $('#start').toggle()
+  $('.gifmiddle').toggle()
+  $('.question').text((whoWon()===3)? "DRAW": "Player " + whoWon()+ " wins!")
+  } else {
+  updateDisplay()
+}
+
   // switch players
   if(quiz.currentPlayer === 1) {
     quiz.currentPlayer = 2
@@ -146,3 +181,57 @@ function restart() {
 // It is assumed that the turns of the player will be automatically changed after each turn.
 //
 // The application will console log all the passed or failed test */
+
+function updateDisplay() {
+$('.gifmiddle').html('<img src=' + questions[currentQuestion()].gif + ' />')
+$('.ansChoice').eq(0).text(questions[currentQuestion()].choices[0])
+$('.ansChoice').eq(1).text(questions[currentQuestion()].choices[1])
+$('.ansChoice').eq(2).text(questions[currentQuestion()].choices[2])
+$('.ansChoice').eq(3).text(questions[currentQuestion()].choices[3])
+$('.question').text(questions[currentQuestion()].question)
+$('.left').text('Player 1: ' + quiz.player1Score)
+$('.right').text('Player 2: ' + quiz.player2Score)
+}
+
+$('#start').click(function() {
+  $('.ansChoice').eq(0).toggle()
+  $('.ansChoice').eq(1).toggle()
+  $('.ansChoice').eq(2).toggle()
+  $('.ansChoice').eq(3).toggle()
+  $('#start').toggle()
+
+  restart()
+  updateDisplay()
+});
+
+$('#a1').click(function() {
+  playTurn(0)
+});
+$('#a2').click(function() {
+  playTurn(1)
+});
+$('#a3').click(function() {
+  playTurn(2)
+});
+$('#a4').click(function() {
+  playTurn(3)
+});
+
+
+// directions on intro page (how many questions can you answer)
+// make answer green if correct answer, see true/false example
+// highlight player 1 or 2 box when it's their turn or just 1 player
+// random pun generation but you only play 10 questions
+// game option #2 singaporean puns
+// timer?
+
+// $('.ansChoice').click(function () {
+//   var choice = parseInt(this.id);
+//   console.log(choice);
+//
+//     console.log(player);
+//   if (playTurn(choice)) {
+//     $(this).text(player)
+//   }
+// });
+});
